443ab256fff62ca09327ec6e87d250df01aee9de
Jonathan Chen <jonchen228@gmail.com>
2022-09-19 22:18:22 -0400

    move runners to swarm

--- drone-stack.deploy.sh
+++ drone-stack.deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+# Stop on error
+set -e
+
+# Deploy server
 WD=$(dirname $0)
 docker-compose --file ${WD}/drone-compose.yml --env-file ${WD}/../.env up -d
+
+# Deploy runners
+SCRIPTS_DIR=${WD}/../../../swarm/scripts
+export ENV_DIR=${WD}/../swarm-env-config
+export ENV_FILE=${ENV_DIR}/swarm.env
+export COMPOSE_FILE=${WD}/drone-swarm.yml
+export STACK_NAME=drone-swarm 
+
+${SCRIPTS_DIR}/stack.deploy.sh
